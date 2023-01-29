6c258ab0a12ef65ca4fffefe6540e8e4703c30db
Victor Caveda <caveda@users.noreply.github.com>
2023-01-29 14:08:46 +0100

    Fixes repository test

--- app/src/test/java/com/victorcaveda/marketwatcher/data/repository/CryptoRepositoryImplTest.kt
+++ app/src/test/java/com/victorcaveda/marketwatcher/data/repository/CryptoRepositoryImplTest.kt
@@ -1,18 +1,42 @@
 package com.victorcaveda.marketwatcher.data.repository
 
+import com.victorcaveda.marketwatcher.data.remote.CryptoCompare
+import com.victorcaveda.marketwatcher.data.remote.model.SymbolPricesDto
+import com.victorcaveda.marketwatcher.data.remote.model.SymbolSinglePrice
+import io.mockk.coEvery
+import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class CryptoRepositoryImplTest {
 
-    @Test
-    fun `Given the crypto ABC When getting the current price Then 9999 is returned`() = runTest {
-        val actual = CryptoRepositoryImpl().getCryptoPrice("ABC")
+    private val cryptoDataSourceDouble = mockk<CryptoCompare>(relaxed = true)
+    private lateinit var sut: CryptoRepositoryImpl
 
-        assertEquals(actual.getOrNull()?.currentPrice, 9999.0)
+    @BeforeEach
+    fun setup() {
+        sut = CryptoRepositoryImpl(cryptoDataSourceDouble)
     }
+
+    @Test
+    fun `Given the crypto ABC When getting the current price Then the current price is returned`() = runTest {
+        val currency = "EUR"
+        val symbol = "ABC"
+        val currentPrice = 9999.0
+        coEvery {
+            cryptoDataSourceDouble.getSingleSymbolPrice(symbol, listOf(currency))
+        } returns buildSymbolPricesResponse(currency, currentPrice)
+
+        val actual = sut.getCryptoPrice(symbol, currency)
+
+        assertEquals(actual.getOrNull()?.currentPrice, currentPrice)
+    }
+
+    private fun buildSymbolPricesResponse(currency: String, currentPrice: Double) =
+        SymbolPricesDto(listOf(SymbolSinglePrice(currency, currentPrice)))
 }
